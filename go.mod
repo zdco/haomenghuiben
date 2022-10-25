df65df092c2a241482fdec1a8b4f9bb0129fead8
chaolv <chaolv@upchina.com>
2022-10-25 19:10:47 +0800

    自动爬取

---
+++ go.mod
@@ -0,0 +1,8 @@
+module zoudaokou.com/haomeng
+
+go 1.19
+
+require (
+	golang.org/x/net v0.1.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"regexp"
+	"strings"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/transform"
+)
+
+var header = map[string]string{
+	"Host":                      "mp.weixin.qq.com",
+	"Connection":                "keep-alive",
+	"Cache-Control":             "max-age=0",
+	"Upgrade-Insecure-Requests": "1",
+	"User-Agent":                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+	"Accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+}
+
+var downloadPrev = "https://res.wx.qq.com/voice/getvoice?mediaid="
+var downloadDir = "media"
+
+func determineEncodings(r io.Reader) []byte {
+	OldReader := bufio.NewReader(r)
+	bytes, err := OldReader.Peek(1024)
+	if err != nil {
+		panic(err)
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	reader := transform.NewReader(OldReader, e.NewDecoder())
+	all, err := ioutil.ReadAll(reader)
+	if err != nil {
+		panic(err)
+	}
+	return all
+}
+
+type StructEvent struct {
+	Key string `json:"key"`
+}
+
+// 定义新的数据类型
+type Spider struct {
+	url    string
+	header map[string]string
+}
+
+// 定义 Spider get的方法
+func (keyword Spider) get_html_header() string {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", keyword.url, nil)
+	if err != nil {
+		panic(err)
+	}
+	for key, value := range keyword.header {
+		req.Header.Add(key, value)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	return string(determineEncodings(resp.Body))
+}
+
+func getUrls() []string {
+	url := "https://mp.weixin.qq.com/s/NOAXaRaMysYV4MwUlulxsQ"
+	spider := &Spider{url, header}
+	html := spider.get_html_header()
+
+	//链接
+	pattern := `href="(.*?)"`
+	rp := regexp.MustCompile(pattern)
+	find_txt := rp.FindAllStringSubmatch(html, -1)
+
+	var urls []string
+	for i := 0; i < len(find_txt); i++ {
+		if strings.Contains(find_txt[i][1], "mp.weixin.qq.com") {
+			urls = append(urls, find_txt[i][1])
+		}
+	}
+
+	return urls
+}
+
+/**
+ * download file from `url`, then write the content to `to`
+ */
+func download(url string, to string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	ioutil.WriteFile(downloadDir+"/"+to, data, 0777)
+	return nil
+}
+
+func downloadAudio(url string) {
+	spider := &Spider{url, header}
+	html := spider.get_html_header()
+
+	//ID
+	pattern0 := `第(.*?)晚`
+	rp0 := regexp.MustCompile(pattern0)
+	find_txt0 := rp0.FindAllStringSubmatch(html, -1)
+
+	//ID
+	pattern1 := `voice_encode_fileid="(.*?)"`
+	rp1 := regexp.MustCompile(pattern1)
+	find_txt1 := rp1.FindAllStringSubmatch(html, -1)
+
+	//名称
+	pattern2 := `<mpvoice .*name="(.*?)"`
+	rp2 := regexp.MustCompile(pattern2)
+	find_txt2 := rp2.FindAllStringSubmatch(html, -1)
+
+	for i := 0; i < len(find_txt1); i++ {
+		fullUrl := downloadPrev + find_txt1[i][1]
+		fileName := find_txt0[i][0] + "." + find_txt2[i][1] + ".mp3"
+		fmt.Println(fullUrl, fileName)
+		download(fullUrl, fileName)
+	}
+
+}
+
+func main() {
+	os.Mkdir(downloadDir, os.ModePerm)
+	urls := getUrls()
+	for i := 0; i < len(urls); i++ {
+		downloadAudio(urls[i])
+	}
+}
